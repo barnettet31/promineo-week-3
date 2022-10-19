36d9834005d4a3f399fec91810e41891d2d0e4b1
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-10-18 20:00:34 -0400

    added explicit naming for some console logs for ease of instructor

--- index.js
+++ index.js
@@ -5,21 +5,20 @@ const subtractFirstFromLast = function(array){
     //takes in an array and then returns the first number in the array from the last number in the array 
 }
 console.log(subtractFirstFromLast(ages));
-ages.push(105);
+ages.push(105); //add new element to end of array
 
-console.log(ages);
-console.log(subtractFirstFromLast(ages));
+console.log('ages with new element pushed ',ages);
+console.log('new last element minus first element ',subtractFirstFromLast(ages));
 
 
 let totalOfAges = 0; 
 
 for(age of ages){
     totalOfAges += age;
-}
-const averageAge = totalOfAges/(ages.length)
-console.log('average of ages', averageAge);
+}//used for loop to demonstrate compentency
+const averageAge = totalOfAges/(ages.length);
+console.log('average of ages ', averageAge);
 //this is the average including my added element 
-
 
 
 const names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'];
@@ -31,16 +30,19 @@ for(person of names){
 const averageLengthOfNames = totalLengthOfNames / names.length;
 console.log('average length of names', averageLengthOfNames);
 
+//want to use a reduce method on the string (I can do the same as I did above with a placeholder string and using += for each iteration)
+
+const concatenatedStrings = names.reduce((acc, nextEl) =>`${acc} ${nextEl}`,'');
+console.log('concatenated strings', concatenatedStrings);
 
 const nameLengths = names.map(element=>element.length);
-console.log(nameLengths);
+console.log('array containing name lengths',nameLengths); //can do the same with a for loop and pushing each length into a placeholder array
+
+const totalOfNameLengths = names.reduce((accumulator, currentValue)=>accumulator + currentValue.length, 0);
+console.log('totalofNameLengths',totalLengthOfNames);
 
 
-const totalOfNameLengths = names.reduce((accumulator, currentValue)=>accumulator + currentValue, 0);
-console.log(totalLengthOfNames);
-
-
-const repeatWords=  (word, n)=>word.repeat(n);
+const repeatWords =  (word, n) => word.repeat(n);
 
 console.log(repeatWords('Hello',3));//prints hello hello hello
 
@@ -49,12 +51,12 @@ const returnFullName = (firstName, lastName)=> `${firstName} ${lastName}`;
 console.log(returnFullName('Travis', 'Barnette'));
 
 const returnSumOfNumberArray = (array)=>array.reduce((acc, nextEl)=>acc+nextEl) > 100;
-console.log(returnSumOfNumberArray([102,101,99,98,97])); // should be true
-console.log(returnSumOfNumberArray([1,2,3])); //should be false
+console.log('example with sum over 100',returnSumOfNumberArray([102,101,99,98,97])); // should be true
+console.log('example with sum under 100',returnSumOfNumberArray([1,2,3])); //should be false
 
-const returnAverageOfArray = (array)=> array.reduce((acc, nextEl)=>acc+nextEl) / array.length;
+const returnAverageOfArray = (array)=> array.reduce((acc, nextEl)=>acc+nextEl,0) / array.length;
 
-console.log(returnAverageOfArray([1,2,3,4,5,6,7,8,9,10])); // should be 5.5
+console.log('average array function', returnAverageOfArray([1,2,3,4,5,6,7,8,9,10])); // should be 5.5
 
 const isOneAverageGreaterThanOther = (arr1, arr2)=> returnAverageOfArray(arr1) > returnAverageOfArray(arr2);
 
@@ -63,7 +65,7 @@ console.log(isOneAverageGreaterThanOther([4,5,6], [1,2,3])); //should be true
 
 //reused function above for ease
 
-const willBuyDrink =  (isHotOutside, moneyInPocket)=> isHotOutside && moneyInPocket > 10.50;
+const willBuyDrink =  (isHotOutside, moneyInPocket) => isHotOutside && moneyInPocket > 10.50;
 
 console.log(willBuyDrink(false, 10.50)); // should be false
 console.log(willBuyDrink(true, 10.51)); // should be true
